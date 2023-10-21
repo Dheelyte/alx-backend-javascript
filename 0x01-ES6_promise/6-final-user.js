035f240bfe41a83598e9634974372b3f934d093a
Dheelyte <olagbujidelight@gmail.com>
2023-10-21 16:55:57 +0000

    6. Import and resolve functions

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from '4-user-promise.js';
+import uploadPhoto from '5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+  .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then(res => (
+    res.map((p) => (
+      {
+        status: p.status,
+        value: p.value === 'fulfilled' ? p.value : String(p.reason)
+      }
+    ))
+  ));
+}
