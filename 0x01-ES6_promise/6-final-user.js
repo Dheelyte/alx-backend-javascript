8aae9128b30ddbaaed8a3b60478af0748ee0408f
Dheelyte <olagbujidelight@gmail.com>
2023-10-21 17:20:56 +0000

    Fix handleProfileSignup function structure

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
-import signUpUser from '4-user-promise.js';
-import uploadPhoto from '5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-  .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-  .then((res) => (
-    res.map((p) => (
-      {
-        status: p.status,
-        value: p.value === 'fulfilled' ? p.value : String(p.reason)
-      }
-    ))
-  ));
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
