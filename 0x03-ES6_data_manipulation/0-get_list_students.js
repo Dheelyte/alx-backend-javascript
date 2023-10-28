7c73c93ab632182d68c87c45140ef2603b8ecb5d
Dheelyte <olagbujidelight@gmail.com>
2023-10-28 11:56:18 +0000

    A Map over students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Delight Olu-Olagbuji <https://github.com/Dheelyte>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
